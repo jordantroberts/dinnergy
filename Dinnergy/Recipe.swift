fb6443de21593479f40545aad5d53210ec70ca2e
Tara Sankhalpara <tsankhalpara@mac.com>
2019-06-14 16:46:45 +0100

    Edited database to include ingredients in Recipe table and add to single recipe view.
    co-authored-by: Nandini <nandini_patel96@hotmail.co.uk>
    co-authored-by: Jordan <jordanroberts@talk21.com>

--- Dinnergy/Recipe.swift
+++ Dinnergy/Recipe.swift
@@ -12,12 +12,14 @@ class Recipe {
     
     var id: Int
     var name: String
+    var ingredients: String
     var method: String
     var attachment: String
     
-    init(id: Int, name: String, method: String, attachment: String){
+    init(id: Int, name: String, ingredients: String, method: String, attachment: String){
         self.id = id
         self.name = name
+        self.ingredients = ingredients
         self.method = method
         self.attachment = attachment
     }
--- Dinnergy/SingleRecipeViewController.swift
+++ Dinnergy/SingleRecipeViewController.swift
@@ -15,24 +15,15 @@ class SingleRecipeViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var methodLabel: UILabel!
+    @IBOutlet weak var ingredientLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     titleLabel.text = recipe[myIndex].name
     methodLabel.text = recipe[myIndex].method
+    ingredientLabel.text = recipe[myIndex].ingredients
     
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
