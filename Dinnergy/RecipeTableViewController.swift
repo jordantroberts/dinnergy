c4d0a67e630ca5340ae23edf7ad9d5648f3dfcc4
Jordan Roberts <jordanroberts@talk21.com>
2019-06-14 11:43:05 +0100

    Retrieved data from Recipes table to display as a list of all recipes
    co-authored-by: Nandini <nandini_patel96@hotmail.co.uk>
    co-authored-by: Tara <tsankhalpara@mac.com>

--- Dinnergy/RecipeTableViewController.swift
+++ Dinnergy/RecipeTableViewController.swift
@@ -10,25 +10,28 @@ import UIKit
 
 class RecipeTableViewController: UITableViewController {
 
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+    var stmt:OpaquePointer?
+    let recipe = instanceDB.showRecipes()
+    
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 12
+        return recipe.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
 
-         cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
-
+         cell.textLabel?.text = recipe[indexPath.row].name
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Section \(section)"
+        return "Recipes"
     }
 
+    
 }
