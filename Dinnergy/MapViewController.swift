283c59a328c799e37a326eb3dca8d8b13d10fce9
Oliver Cripps <ollycripps@gmail.com>
2019-06-17 16:11:27 +0100

    Add segemented control functions to search and display by label name
    co-authored-by: SimonChenboYi <yichenbo@outlook.com>

--- Dinnergy/MapViewController.swift
+++ Dinnergy/MapViewController.swift
@@ -29,6 +29,34 @@ class MapViewController: ViewController, MKMapViewDelegate {
         let coordinateRegion = MKCoordinateRegion.init(center: initialLocation.coordinate, latitudinalMeters: searchRadius * 1.5, longitudinalMeters: searchRadius * 1.5)
         mapView.setRegion(coordinateRegion, animated: true)
         
+        searchInMap()
+    }
+    
+    func searchInMap() {
+        // 1 MKLocalSearchRequest object is created and can be used to specify search parameters. The search query is set to the title of the currently selected index of the segment control
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)
+        // 2 The region on which the search will be applied is defined
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        request.region = MKCoordinateRegion(center: initialLocation.coordinate, span: span)
+        // 3 MLLocalSearchObject initiates thes earch operation and deliver the results back into an array of MKMapItems. This will contain the name, latitide and longitute of the current POI
+        let search = MKLocalSearch(request: request)
+        search.start(completionHandler: {(response, error) in
+            for item in response!.mapItems {
+                self.addPinToMapView(title: item.name, latitude: item.placemark.location!.coordinate.latitude, longitude: item.placemark.location!.coordinate.longitude)
+            }
+        })
+    }
+    
+    func addPinToMapView(title: String?, latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        if let title = title {
+            let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            annotation.title = title
+            
+            mapView.addAnnotation(annotation)
+        }
     }
     
     /*
@@ -45,8 +73,8 @@ class MapViewController: ViewController, MKMapViewDelegate {
     
     @IBAction func searchOnValueChanged(_ sender: Any) {
         
-        
-        
+        mapView.removeAnnotations(mapView.annotations)
+        searchInMap()
         
     }
     
