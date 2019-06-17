86d8800b19dd9d52df2d471798c543a8c17613ad
Chenbo Yi <yichenbo@outlook.com>
2019-06-17 15:07:58 +0100

    add the map view and controller
    co-authored-by: Oliver <ollycripps@gmail.com>

---
+++ Dinnergy/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  Dinnergy
+//
+//  Created by Simon 易 on 17/06/2019.
+//  Copyright © 2019 Edgy Eats. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
