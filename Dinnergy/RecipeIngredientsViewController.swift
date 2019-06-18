1d58b4c12e4bdfca93f9919ce1b52d96cbfec4e2
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-06-18 14:04:37 +0100

    created recipeingredients controller and linked to view
    co-authored-by: Tara <tsankhalpara@mac.com>

---
+++ Dinnergy/RecipeIngredientsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeIngredientsViewController.swift
+//  Dinnergy
+//
+//  Created by Nandini Patel on 18/06/2019.
+//  Copyright © 2019 Edgy Eats. All rights reserved.
+//
+
+import UIKit
+
+class RecipeIngredientsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
