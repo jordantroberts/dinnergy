a1d340767cb0a3ac4486acef459aff695cb79d0f
Carly <carly.jenkinson65@googlemail.com>
2019-06-18 16:02:57 +0100

    Add test for map view supermarket button
    co-authored-by: SimonChenboYi <yichenbo@outlook.com>

--- DinnergyUITests/MapViewFeatureTest.swift
+++ DinnergyUITests/MapViewFeatureTest.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Edgy Eats. All rights reserved.
 //
 
-import XCTest
+import Foundation
 import XCTest
 
 class MapViewFeatureTest: XCTestCase {
@@ -16,9 +16,14 @@ class MapViewFeatureTest: XCTestCase {
         app.tabBars.buttons["Foodbank"].tap()
         XCTAssert(app/*@START_MENU_TOKEN@*/.segmentedControls.buttons["Food Bank"]/*[[".segmentedControls.buttons[\"Food Bank\"]",".buttons[\"Food Bank\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.exists)
         app.buttons["Ingredient"].tap()
-        
-        
-
+    }
+    
+    func test2MatchMap() {
+        let app = XCUIApplication()
+        app.tabBars.buttons["Ingredient"].tap()
+        app.tabBars.buttons["Foodbank"].tap()
+        XCTAssert(app.segmentedControls.buttons["Supermarket"].exists)
+        app.buttons["Ingredient"].tap()
     }
 
 }
