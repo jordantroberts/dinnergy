1f53249b10bb1f889005ab7b6820d89e8e2a6384
Chenbo Yi <yichenbo@outlook.com>
2019-06-18 13:01:43 +0100

    feature testing map page
    co-authored-by: Jordan <jordanroberts@talk21.com>

--- DinnergyUITests/MapViewFeatureTest.swift
+++ DinnergyUITests/MapViewFeatureTest.swift
@@ -11,6 +11,14 @@ import XCTest
 
 class MapViewFeatureTest: XCTestCase {
 
+    func testMatchMap() {
+        let app = XCUIApplication()
+        app.tabBars.buttons["Foodbank"].tap()
+        XCTAssert(app/*@START_MENU_TOKEN@*/.segmentedControls.buttons["Food Bank"]/*[[".segmentedControls.buttons[\"Food Bank\"]",".buttons[\"Food Bank\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.exists)
+        app.buttons["Ingredient"].tap()
+        
+        
 
+    }
 
 }
