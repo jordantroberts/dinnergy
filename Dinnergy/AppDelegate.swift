6050a6be9238210e927eed8a1984634594c74cfd
Oliver Cripps <ollycripps@gmail.com>
2019-06-19 15:05:34 +0100

    Update title bar color to white
    co-authored-by: Jordan <jordanroberts@talk21.com>

--- Dinnergy/AppDelegate.swift
+++ Dinnergy/AppDelegate.swift
@@ -13,14 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let attributes = [NSAttributedString.Key.font: UIFont(name: "AvenirNext-Bold", size: 17)!]
         UINavigationBar.appearance().titleTextAttributes = attributes
         UIBarButtonItem.appearance().setTitleTextAttributes(attributes, for: .normal)
+        var navigationBarAppearance = UINavigationBar.appearance()
+        navigationBarAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
         return true
-        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
