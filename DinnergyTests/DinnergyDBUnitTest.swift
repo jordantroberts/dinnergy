c2f89ee8bfccb17d2dae60fb96d3fbf4b9ead25d
Chenbo Yi <yichenbo@outlook.com>
2019-06-20 15:55:42 +0100

    Test the dropTable function

--- DinnergyTests/DinnergyDBUnitTest.swift
+++ DinnergyTests/DinnergyDBUnitTest.swift
@@ -11,6 +11,26 @@ import XCTest
 
 class DinnergyDBUnitTest: XCTestCase {
     
+    func testInsertIngredients() {
+        
+        instanceDB.insertIngredients(name: "Banana", quantity: 300.0, unit: "g")
+        
+        let ingredient = instanceDB.checkStock().last
+        XCTAssertEqual(ingredient!.name, "Banana")
+        
+        instanceDB.deleteItem(name: "Banana")
+    }
+    
+    func testDropTable() {
+        
+        instanceDB.insertIngredients(name: "Banana", quantity: 300.0, unit: "g")
+        instanceDB.dropTable()
+        instanceDB.createTable()
+        instanceDB.insertIngredients(name: "Egg", quantity: 5 , unit: "")
+        let ingredient = instanceDB.checkStock().first
+        XCTAssertEqual(ingredient!.name, "Egg")
+        instanceDB.deleteItem(name: "Egg")
+    }
    
     
 }
