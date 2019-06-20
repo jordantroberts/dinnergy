4539c162627ece6b649e81b8068e47b9c00ef1d9
Jordan Roberts <jordanroberts@talk21.com>
2019-06-20 14:07:41 +0100

    Added feature tests for map button and for clear function

--- DinnergyUITests/MapViewFeatureTest.swift
+++ DinnergyUITests/MapViewFeatureTest.swift
@@ -17,8 +17,14 @@ class MapViewFeatureTest: XCTestCase {
         XCTAssert(app/*@START_MENU_TOKEN@*/.segmentedControls.buttons["Food Bank"]/*[[".segmentedControls.buttons[\"Food Bank\"]",".buttons[\"Food Bank\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.exists)
         app.buttons["Ingredients"].tap()
         
+    }
+    
+    func testMapSupermarkets() {
+        let app = XCUIApplication()
+        app.tabBars.buttons["Nearby"].tap()
+        XCTAssert(app.segmentedControls.buttons["Supermarket"].exists)
+        app.buttons["Ingredients"].tap()
         
-
     }
 
 }
--- DinnergyUITests/ShoppingListItemFeatureTest.swift
+++ DinnergyUITests/ShoppingListItemFeatureTest.swift
@@ -42,6 +42,25 @@ class ShoppingListItemFeatureTest: XCTestCase {
         
     }
     
+    func testClearItem() {
+        let app = XCUIApplication()
+        
+        addBananas(number: "3")
+        app.buttons["Recipes"].tap()
+        app.tables.element(boundBy: 0).cells.element(boundBy: 0).tap()
+        
+        let scrollViewsQuery = app.scrollViews
+        let veganBananaBreadElement = scrollViewsQuery.otherElements.containing(.staticText, identifier:"Vegan Banana Bread").element
+        veganBananaBreadElement.swipeUp()
+        
+        app.buttons["Add to List"].tap()
+        app.alerts["Added to List"].buttons["Dismiss"].tap()
+        app.tabBars.buttons["Shopping"].tap()
+        app.buttons["Clear"].tap()
+        XCTAssertFalse(app.staticTexts["Bananas: 3.0 large"].exists)
+
+        
+    }
 
 
 }
